8652533644ef5c334586bfb50ab434153ed329dc
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-11-09 20:53:45 -0400

    backrest: Use waitforssh to wait before downloading the asset

--- backrest/wait-and-download.sh
+++ backrest/wait-and-download.sh
@@ -6,27 +6,12 @@ BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
 . "${BASEDIR}/.env"
 
 OUTPUT="${OUTPUT:-${PWD}}"
-WAIT="${WAIT:-10}"
 
 test -n "${ASSET}" || exit $?
 
-while :; do
-    ssh -o LogLevel=QUIET \
-        -o StrictHostKeyChecking=no \
-        -o UserKnownHostsFile=/dev/null \
-        -o User=user \
-        "${BACKREST_HOSTNAME}" test -f "/${ASSET}"
-
-    if [ $? -ne 0 ]; then
-        sleep "${WAIT}" || exit $?
-        continue
-    fi
-
+waitforssh -T "test -f /${ASSET}" -- "user@${BACKREST_HOSTNAME}" &&
     scp -o LogLevel=VERBOSE \
         -o StrictHostKeyChecking=no \
         -o UserKnownHostsFile=/dev/null \
         -o User=user \
         "${BACKREST_HOSTNAME}:/${ASSET}" "${OUTPUT}"
-
-    exit $?
-done
